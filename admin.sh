5661876392165db64ba9a27940374eb81ccb1f33
SuryaAtchyuth <137221914+SuryaAtchyuth@users.noreply.github.com>
2023-09-28 23:34:28 -0500

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -3,5 +3,5 @@ echo
 echo  "     Project SimOS."
 echo
 echo  "                   Developed by"
-echo  "         SXV210094 - Surya Atchyuth, Veerubhotla"
+echo  "           Surya Atchyuth, Veerubhotla"
 echo
